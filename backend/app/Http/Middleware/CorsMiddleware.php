a42b3b1d6b35b0639ca81b9baae42d88fc755022
Yoku Araki <ay.araki0819@outlook.jp>
2025-02-04 19:57:43 +0800

    follows

--- backend/app/Http/Middleware/CorsMiddleware.php
+++ backend/app/Http/Middleware/CorsMiddleware.php
@@ -12,9 +12,13 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        // CORSヘッダーを設定
-        $response->headers->set('Access-Control-Allow-Origin', 'http://127.0.0.1:5173'); // または許可するオリジン
+        $allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5173']; // 許可するオリジン
 
+    $origin = $request->headers->get('Origin');
+
+    if (in_array($origin, $allowedOrigins)) {
+        $response->headers->set('Access-Control-Allow-Origin', $origin);
+    }
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
