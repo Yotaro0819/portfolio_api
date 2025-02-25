2326072b3bb5062774c3599306329358b48360df
Yoku Araki <ay.araki0819@outlook.jp>
2025-02-26 00:41:01 +0800

    not yet

--- backend/app/Http/Controllers/PaymentController.php
+++ backend/app/Http/Controllers/PaymentController.php
@@ -12,7 +12,15 @@ class PaymentController extends Controller
     {
         $user = JWTAuth::parseToken()->authenticate();
 
-        $orders = Payment::where('payer_id', $user->id)->where('process_status', 'pending')->with('payer:id,name')->get();
+        $orders = Payment::where(function ($query) use ($user) {
+            $query->where('seller_id', $user->id)
+                ->orWhere('payer_id', $user->id);
+        })
+        ->where(function ($query) {
+            $query->where('process_status', 'approved')
+                ->orWhere('process_status', 'pending');
+        })
+        ->with('payer:id,name')->get();
 
         return response()->json(['orders' => $orders]);
     }
