7733f21b1db971ef7ab88d3df2ed80862e8a3eeb
Yoku Araki <ay.araki0819@outlook.jp>
2025-04-04 03:29:12 +0900

    deploy

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,7 +9,7 @@ function getCookie(name) {
 }
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'd39hmozy4wec8b.cloudfront.net',
   timeout: 10000,
   withCredentials: true,  // クッキーを送信するため
 });
@@ -46,8 +46,8 @@ axiosInstance.interceptors.response.use(
         const newAccessToken = refreshResponse.data.newAccessToken;
 
         if (!newAccessToken) {
-          localStorage.removeItem('authUser');
-          window.location.href = '/login';
+          // localStorage.removeItem('authUser');
+          // window.location.href = '/login';
           throw new Error('New access token is undefined');
         }
 
