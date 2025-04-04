d40e9c2043c3438af0044df6cba834366e43a77d
Yoku Araki <ay.araki0819@outlook.jp>
2025-04-04 18:26:42 +0900

    deploy

--- backend/app/Http/Middleware/VerifyXSRFToken.php
+++ backend/app/Http/Middleware/VerifyXSRFToken.php
@@ -10,20 +10,20 @@ class VerifyXSRFToken
 {
     public function handle(Request $request, Closure $next)
     {
-        $token = $request->header('X-XSRF-TOKEN');
-        $csrfCookie = $request->cookie('XSRF-TOKEN');
+        // $token = $request->header('X-XSRF-TOKEN');
+        // $csrfCookie = $request->cookie('XSRF-TOKEN');
 
-        if ($token !== $csrfCookie) {
-            // CSRFトークンが一致しない場合のエラーログ
-            Log::error('CSRF token mismatch or missing.', [
-                'token' => $token,
-                'csrf_cookie' => $csrfCookie
-            ]);
+        // if ($token !== $csrfCookie) {
+        //     // CSRFトークンが一致しない場合のエラーログ
+        //     Log::error('CSRF token mismatch or missing.', [
+        //         'token' => $token,
+        //         'csrf_cookie' => $csrfCookie
+        //     ]);
 
-            // JSONレスポンスを返す
-            return response()->json(['message' => 'CSRF Token mismatch or missing.'], 403);
-        }
+        //     // JSONレスポンスを返す
+        //     return response()->json(['message' => 'CSRF Token mismatch or missing.'], 403);
+        // }
 
-        return $next($request);
+        // return $next($request);
     }
 }
