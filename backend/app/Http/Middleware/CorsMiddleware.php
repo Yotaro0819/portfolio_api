425dbf0bb338d6ccdad058d6e141ec7e0feb821c
Yoku Araki <ay.araki0819@outlook.jp>
2025-04-27 21:53:08 +0900

    cookie helper

--- backend/app/Http/Middleware/CorsMiddleware.php
+++ backend/app/Http/Middleware/CorsMiddleware.php
@@ -31,7 +31,6 @@ class CorsMiddleware
 
         // OPTIONSリクエスト（プリフライトリクエスト）に対しては200を返す
         if ($request->getMethod() == "OPTIONS") {
-            // return response('', 200);
             return response('', 200)
           ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
           ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Accept, Origin')
