9fecc740039e4b59167e9a5d757e1129c90f4715
Yoku Araki <ay.araki0819@outlook.jp>
2025-04-27 12:25:08 +0900

    コード整理

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Follow;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserController extends Controller
